38097970b0b0853d2fc118a4086357371686f783
xp-squared <108478666+xp-squared@users.noreply.github.com>
2023-10-12 19:29:58 -0400

    Main.cpp

--- Vehicle Rental System/Main.cpp
+++ Vehicle Rental System/Main.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 // NOTES  *********************
 // fix beginning input, if you enter a string at entry screen it becomes infinite output
+// in user and admin, we have the password and username section, possibly add to it where you can change the details of each
 
 void entryScreen() {
     cout << "Welcome to Zane's Vehicle Rental System!" << endl;
@@ -14,7 +15,7 @@ void entryScreen() {
     cout << "2. User Login" << endl;
     cout << "3. Create New User" << endl;
     cout << "4. Exit" << endl;
-    cout << "Please enter your choice (1-3): ";
+    cout << "Please enter your choice (1-4): ";
 }
 
 int getValidatedChoice() {
@@ -22,7 +23,7 @@ int getValidatedChoice() {
     cin >> choice;
     while (choice < 1 || choice > 4) {
         cout << "Invalid choice. Please try again.";
-        cout << " Please enter your choice (1-3): ";
+        cout << " Please enter your choice (1-4): ";
         cin >> choice;
         if (choice < 1 || choice > 4) {
             cout << "Invalid choice. Please try again." << endl;
@@ -56,10 +57,23 @@ int main() {
         }
 
         // login as user
-        /*if (choice == 2) {
-            Admin adminObj;
+        else if (choice == 2) {
+            User userObj;
             string username, password;
-        }*/
+            cout << "Enter UserName: ";
+            cin >> username;
+            cout << "Enter Password: ";
+            cin >> password;
+
+            if (userObj.login(username, password)) {
+                cout << "\nUser login successful!\n" << endl;
+                userObj.userMenu();
+            }
+            else {
+                cout << "\nLogin failed.\n" << endl;
+            }
+        }
+
 
         // create new user
         /*if (choice == 3) {
