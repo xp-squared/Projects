8483fa2b7e751592d5e97e5769454165c949a606
xp-squared <108478666+xp-squared@users.noreply.github.com>
2023-10-12 19:30:33 -0400

    User.h

--- Vehicle Rental System/User.h
+++ Vehicle Rental System/User.h
@@ -1,10 +1,14 @@
 #pragma once
+#include <string>
+using namespace std;
 
 class User {
 public:
-
+	bool login(const string &username, const string &password);
+	void userMenu();
 
 private:
-
+	string password;
+	string username;
 
 };
